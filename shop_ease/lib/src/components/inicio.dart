e8c963fdff5ebac01f8b8f076621045594c4f128
Isadora-R <d2022015952@unifei.edu.br>
2023-12-01 02:33:56 -0300

    TelaDeInicioEProdutos

--- shop_ease/lib/src/components/inicio.dart
+++ shop_ease/lib/src/components/inicio.dart
@@ -1,14 +1,89 @@
 ﻿import 'package:flutter/material.dart';
+import '../model/products_model.dart';
+import '../service/products_service.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'produto_clicado.dart';
 
-class Inicio extends StatelessWidget {
+class Inicio extends StatefulWidget {
   const Inicio({super.key});
+  @override
+  // ignore: library_private_types_in_public_api
+  _InicioState createState() => _InicioState();
+}
 
+class _InicioState extends State<Inicio> {
+  late Future<Welcome> produtosFuture;
+  @override
+  void initState() {
+    super.initState();
+    produtosFuture = getProducts();
+  }
+
+  // https://api.flutter.dev/flutter/widgets/ConnectionState.html
+
+  // https://pub.dev/packages/carousel_slider/example
+
+  //se tirar os ifs a tela da erro enquanto a api carrega
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Página Inicial',
-        style: TextStyle(fontSize: 24.0),
+    return Scaffold(
+      body: FutureBuilder<Welcome>(
+        future: produtosFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Algo deu errado: ${snapshot.error}'));
+          } else {
+            List<Product> products = snapshot.data!.products;
+            return Center(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 1000),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: products.map((product) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    ProdutoClicado(product: product)),
+                          );
+                          print('Item clicado: ${product.title}');
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 8.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.purple,
+                          ),
+                          child: Image.network(
+                            product.thumbnail,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            );
+          }
+        },
       ),
     );
   }
---
+++ shop_ease/lib/src/components/produto_clicado.dart
@@ -0,0 +1,82 @@
+﻿import 'package:flutter/material.dart';
+import 'package:shop_ease/src/model/products_model.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+//import 'inicio.dart';
+
+class ProdutoClicado extends StatefulWidget {
+  final Product product;
+  const ProdutoClicado({super.key, required this.product});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ProdutoClicadoState createState() => _ProdutoClicadoState();
+}
+
+class _ProdutoClicadoState extends State<ProdutoClicado> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhes do Produto'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(0.5),
+              child: Text(
+                widget.product.title,
+                style: const TextStyle(fontSize: 30.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.4,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 1000),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: widget.product.images
+                    .map((item) => Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 4.0, vertical: 60.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.purple,
+                          ),
+                          child: Image.network(
+                            item,
+                            fit: BoxFit.contain,
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+            Text(
+              'Preço: R\$ ${widget.product.price}',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            const Text(
+              'Descrição',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              widget.product.description,
+              style: const TextStyle(fontSize: 15.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
